a8f1e91c7d0a1550be4d4d07830436d6636d9781
Rishabh <727724euit196@skcet.ac.in>
2025-11-09 16:13:25 +0530

    Delete Rishabh.V_Submission/urls.py

--- Rishabh.V_Submission/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, GenerateMusicViewSet
-
-router = DefaultRouter()
-router.register(r'user', UserViewSet, basename='user')
-router.register(r'music', GenerateMusicViewSet, basename='music')
-
-urlpatterns = router.urls
