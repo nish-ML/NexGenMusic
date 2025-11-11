3b3a98deba015533fa41060ed9a7a5b307d4250a
SHAHRUKESH DM <dmshahru99@gmail.com>
2025-11-11 22:37:21 +0530

    Delete Milestone 2 - Shahrukesh DM (Backend) directory

--- Milestone 2 - Shahrukesh DM (Backend)/mood_api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import GeneratedMusic
-
-admin.site.register(GeneratedMusic)
-
--- Milestone 2 - Shahrukesh DM (Backend)/mood_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import GeneratedMusic
-
-class GeneratedMusicSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GeneratedMusic
-        fields = '__all__'
